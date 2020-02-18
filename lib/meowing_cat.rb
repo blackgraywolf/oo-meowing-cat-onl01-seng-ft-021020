1991eadc16f586ca0cd513a62434fb0fb05dd2c5
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-02-18 03:33:47 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,13 +1,9 @@
 ## code your solution here. 
 class Cat
-  attr_accessor :meow
-  def initialize(meow)
-    @meow = meow
-  
-  end
-  
-  
+  attr_accessor :name
+
   def meow
     puts "meow!"
-  end
+end
+
 end
