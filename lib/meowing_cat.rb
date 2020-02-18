1bfe5626a10dda9e514b6087dcc92fc7f872a0ae
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-02-18 03:29:01 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,14 @@
 ## code your solution here. 
+Class Cat
+  attr_accessor :name
+  attr_reader :age
+  
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+  
+  def say_name
+    puts "Hi, my name is #{@name}"
+  end
+end
